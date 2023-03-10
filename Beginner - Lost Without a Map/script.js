142ea7d4e37dbcc28afb7f4c9c08c6f8dc7e5740
Giulio Simone Floresta <florestagiuliosimone@gmail.com>
2023-03-10 13:34:52 +0100

    Add files via upload

---
+++ Beginner - Lost Without a Map/script.js
@@ -0,0 +1,11 @@
+function maps(x){
+  
+    let arrayDouble = [];
+    
+    for(let i=0; i<x.length; i++){
+      const mathDouble = x[i] * 2;
+      arrayDouble.push(mathDouble);
+    }
+    
+    return arrayDouble;
+  }
