d5d47a018920ac3f32dfae105a501f781cd7b720
Giulio Simone Floresta <florestagiuliosimone@gmail.com>
2023-03-10 18:56:34 +0100

    Add files via upload

---
+++ Calculate BMI/script.js
@@ -0,0 +1,7 @@
+function bmi(weight, height) {
+    let bmi = weight/(height*height)
+    if(bmi <= 18.5) return "Underweight"
+    if(bmi <= 25.0) return "Normal"
+    if(bmi <= 30.0) return "Overweight"
+    if(bmi > 30) return "Obese"
+  }
