f26f7dd6ea4b841f5cbe1a4892a37c611fc40a14
Giulio Simone Floresta <florestagiuliosimone@gmail.com>
2023-03-21 08:56:11 +0100

    Update script.js

--- Sum Arrays/script.js
+++ Sum Arrays/script.js
@@ -1,8 +1,13 @@
 function sum (numbers) {
-    "use strict";
-    let sum = 0;
+  let sum = 0;
+  
+  if(numbers.length < 1){
+    return 0;
+  } else{
     for(let i = 0; i < numbers.length; i++){
-        sum = sum + numbers[i];
+      sum = sum+numbers[i];
     }
-    return sum;
-}
+  
+  return sum;
+  }    
+};
